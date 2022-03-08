56e5444536b42e69f366022a447a485f96ee288b
ryoka.terada <ryoka.terada@careritz.co.jp>
2022-03-08 12:37:36 +0900

    プレイヤー名を変更可能にした

--- components/GameBoard.vue
+++ components/GameBoard.vue
@@ -1,9 +1,27 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <h5 v-if="player">Next player is 〇</h5>
-      <h5 v-if="!player">Next player is ×</h5>
+      <v-col>
+        <v-text-field
+          v-model="playerName1"
+          label="プレイヤー名を入力してください(先攻)"
+        ></v-text-field>
+      </v-col>
+      <span>VS</span>
+      <v-col>
+        <v-text-field
+          v-model="playerName2"
+          label="プレイヤー名を入力してください(後攻)"
+        ></v-text-field>
+      </v-col>
     </v-row>
+    <v-row>
+      <p class="explain">マスに表示されるのはプレイヤー名の最初の２文字です</p>
+    </v-row>
+    <v-row justify="center">
+      <h5>Next player is {{ nextPlayer }}</h5>
+    </v-row>
+    <v-row></v-row>
     <v-row
       v-for="i of boardsize" :key="i"
       justify="center"
@@ -11,7 +29,7 @@
       <Square
         v-for="j of boardsize" :key="j"
         :value="status[j+(i*boardsize)-boardsize-1]"
-        @changePlayer="changePlayer(j+(i*boardsize)-boardsize-1)"
+        @changeTurn="changeTurn(j+(i*boardsize)-boardsize-1)"
       ></Square>
     </v-row>
   </v-container>
@@ -32,13 +50,18 @@ export default class GameBoard extends Vue {
 
   // 各マスのステータスを保持
   status: string[] = [];
+  // プレイヤー名
+  playerName1: string = "〇";
+  playerName2: string = "×";
   // プレイヤーの手番を管理
-  player: Boolean = true;
+  turn: Boolean = true;
+  nextPlayer: String = this.playerName1;
 
   created() {
     this.makeStatusArray();
   }
 
+  // ステータス配列を再生成
   @Watch("boardsize", {deep:true})
   makeStatusArray(){
     this.status = [];
@@ -47,9 +70,34 @@ export default class GameBoard extends Vue {
     }
   }
 
-  changePlayer(num: number){
-    this.player? this.status.splice(num,1,"〇") : this.status.splice(num,1,"×");
-    this.player = !this.player;
+  // プレイヤー名は入力された最初の2文字
+  @Watch("playerName1", {deep:true})
+  @Watch("playerName2", {deep:true})
+  cutName(){
+    this.playerName1 = this.playerName1.substring(0,2);
+    this.playerName2 = this.playerName2.substring(0,2);
+  }
+
+  // ステータスを登録してターン変更
+  changeTurn(num: number){
+    this.turn? this.status.splice(num,1,this.playerName1) : this.status.splice(num,1,this.playerName2);
+    this.turn = !this.turn;
+    this.nextPlayer = this.turn? this.playerName1 : this.playerName2;
   }
 }
 </script>
+
+<style scoped>
+span {
+  display: inline-flex;
+  margin: 20px;
+  margin-top: 20px;
+}
+p.explain{
+  font-size: 15px;
+  padding-left: 12px;
+}
+.col{
+  padding-bottom: 1px;
+}
+</style>
--- components/atoms/Square.vue
+++ components/atoms/Square.vue
@@ -13,7 +13,7 @@ export default class GameBoard extends Vue {
   value: String;
 
   select(){
-    this.$emit("changePlayer");
+    this.$emit("changeTurn");
   }
 }
 </script>
